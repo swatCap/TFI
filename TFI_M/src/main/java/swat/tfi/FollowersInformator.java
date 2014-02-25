9aba75dcf3bdba3ae04c1ac378ad7ed88466df13
svyat <svyat@vadim-mbp.(none)>
2014-02-25 19:12:22 +0200

    Initial commit

---
+++ TFI_M/src/main/java/swat/tfi/FollowersInformator.java
@@ -0,0 +1,23 @@
+/*
+ *  Copyright (c) ParkCode 2004 - 2013
+ *  all rights reserved
+ */
+
+package swat.tfi;
+
+/**
+ * <b>Предназначение:</b><br/>
+ *   <p></p>
+ *
+ * <br/><b>Описание:</b><br/>
+ *   <p></p>
+ * 
+ * <br/>Создан Feb 25, 2014, 6:32:33 PM<br/>
+ *
+ * @author Sviatoslav Lobach (s.lobach@parkcode.com.ua)
+ */
+
+public interface FollowersInformator
+{
+    public IDs getMyFollowers();
+}
